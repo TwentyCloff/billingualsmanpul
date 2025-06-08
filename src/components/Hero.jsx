ebeef965b56709522cef1ff783e875f552ba7ec6
TwentyCloff <twencloff@gmail.com>
2025-06-08 12:44:51 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import Typewriter from "typewriter-effect";
 import { curve } from "../assets";
-import blackholeVideo from "../assets/hero/animated.webm";
+import blackholeVideo from "../assets/hero/animated4.webm";
 import Button from "./Button";
 import Section from "./Section";
 
